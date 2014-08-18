af9b8f56c98dbdb73f0728f73135abcb4955aedd
M Andri Eka F <13511088@std.stei.itb.ac.id>
2014-08-18 13:51:12 +0700

    add new db

--- TestingManagement/testingdb.sql
+++ TestingManagement/testingdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2014 at 05:41 AM
+-- Generation Time: Aug 18, 2014 at 08:44 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -27,8 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `data` (
-  `No` int(11) NOT NULL AUTO_INCREMENT,
-  `ID` int(11) NOT NULL,
+  `dataID` int(11) NOT NULL AUTO_INCREMENT,
+  `no` int(11) NOT NULL,
   `Stream` text NOT NULL,
   `Scenario` text NOT NULL,
   `TestCase` text NOT NULL,
@@ -41,14 +41,14 @@ CREATE TABLE IF NOT EXISTS `data` (
   `Remark` text,
   `DefectID` int(11) DEFAULT NULL,
   `FinalStatus` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`No`)
+  PRIMARY KEY (`dataID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`No`, `ID`, `Stream`, `Scenario`, `TestCase`, `TesterWipro`, `TesterTsel`, `Cycle`, `PlannedStartDate`, `PlannedEndDate`, `Status`, `Remark`, `DefectID`, `FinalStatus`) VALUES
+INSERT INTO `data` (`dataID`, `no`, `Stream`, `Scenario`, `TestCase`, `TesterWipro`, `TesterTsel`, `Cycle`, `PlannedStartDate`, `PlannedEndDate`, `Status`, `Remark`, `DefectID`, `FinalStatus`) VALUES
 (1, 13, 'qewffe', 'wefwf', 'wefef', 'wfwefw', 'ewfewf', 13, '2014-08-12', '2014-08-27', 0, 'ewffw', 123, 0),
 (2, 10, 'dfseef', 'sfsf', 'sfsf', 'sfse', 'sefsef', 1, '2014-08-01', '2014-08-08', 0, 'sffsesegseg', 7, 0),
 (3, 1312, 'sesuatu', 'test', 'sesuatu aja', 'someone (?)', 'emon', 1, '2014-08-22', '2014-08-01', 0, NULL, NULL, 0),
@@ -62,24 +62,26 @@ INSERT INTO `data` (`No`, `ID`, `Stream`, `Scenario`, `TestCase`, `TesterWipro`,
 --
 
 CREATE TABLE IF NOT EXISTS `log` (
-  `No` int(11) NOT NULL AUTO_INCREMENT,
-  `ID` int(11) NOT NULL,
+  `logID` int(11) NOT NULL AUTO_INCREMENT,
+  `Ndata` int(11) NOT NULL,
   `TanggalTest` date NOT NULL,
   `Keterangan` text NOT NULL,
-  PRIMARY KEY (`No`),
+  PRIMARY KEY (`logID`),
   UNIQUE KEY `TanggalTest` (`TanggalTest`),
-  KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  KEY `ID` (`Ndata`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `log`
 --
 
-INSERT INTO `log` (`No`, `ID`, `TanggalTest`, `Keterangan`) VALUES
+INSERT INTO `log` (`logID`, `Ndata`, `TanggalTest`, `Keterangan`) VALUES
 (1, 1, '2014-08-20', '1eqqdd'),
 (2, 1, '2014-08-01', 'safsafas'),
 (8, 1, '2014-08-02', 'eergerg'),
-(9, 2, '2014-08-23', 'sesutu');
+(9, 2, '2014-08-23', 'sesutu'),
+(10, 1, '2014-08-14', 'sesuatu'),
+(13, 1, '2014-08-15', 'sesuatu');
 
 --
 -- Constraints for dumped tables
@@ -89,7 +91,8 @@ INSERT INTO `log` (`No`, `ID`, `TanggalTest`, `Keterangan`) VALUES
 -- Constraints for table `log`
 --
 ALTER TABLE `log`
-  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `data` (`No`);
+  ADD CONSTRAINT `log_ibfk_2` FOREIGN KEY (`Ndata`) REFERENCES `data` (`dataID`),
+  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`Ndata`) REFERENCES `data` (`dataID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
